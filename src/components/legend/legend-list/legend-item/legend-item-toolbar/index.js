b6a770aa0bca54faba4e447f9d02a1f11eb57d15
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2018-03-08 10:35:18 +0100

    Legend: show custom toolbar only if it has children

--- src/components/legend/legend-list/legend-item/legend-item-toolbar/index.js
+++ src/components/legend/legend-list/legend-item/legend-item-toolbar/index.js
@@ -16,17 +16,35 @@ import styles from './styles.scss';
 export class LegendItemToolbar extends PureComponent {
   static propTypes = {
     // Props
-    children: PropTypes.any.isRequired
+    children: PropTypes.node
+  }
+
+  static defaultProps = {
+    // Props
+    children: []
   }
 
   render() {
+    const { children } = this.props;
+
     return (
       <div styleName="c-legend-item-toolbar">
-        {React.Children.map(this.props.children, c =>
-          React.cloneElement(
-            c,
-            { ...this.props }
-          ))}
+        {!!React.Children.count(children) &&
+          React.Children.map(children, c =>
+            React.cloneElement(
+              c,
+              { ...this.props }
+            ))
+        }
+
+        {/* If there is no children defined, let's use the components we had */}
+        {!React.Children.count(children) && <LegendItemButtonBBox {...this.props} />}
+        {!React.Children.count(children) && <LegendItemButtonLayers {...this.props} />}
+        {!React.Children.count(children) && <LegendItemButtonOpacity {...this.props} />}
+        {!React.Children.count(children) && <LegendItemButtonVisibility {...this.props} />}
+        {!React.Children.count(children) && <LegendItemButtonInfo {...this.props} />}
+        {!React.Children.count(children) && <LegendItemButtonRemove {...this.props} /> }
+
       </div>
     );
   }
