ec7b56f0acf684268e133854201a287f2a5a1889
David Inga <david.inga@vizzuality.com>
2018-08-17 16:24:07 +0200

    added support for css files

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,21 @@ const config = {
         exclude: /node_modules/
       },
       {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              includePaths: ['./node_modules']
+                .map(d => path.join(__dirname, d))
+                .map(g => glob.sync(g))
+                .reduce((a, c) => a.concat(c), [])
+            }
+          }
+        ]
+      },
+      {
         test: /\.scss$/,
         use: [
           {
