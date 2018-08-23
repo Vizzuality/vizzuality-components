dd222c961a66ca23357479f6b951813485f45a43
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2018-08-23 19:05:13 +0200

    Spinner: shrinking

--- src/components/spinner/index.js
+++ src/components/spinner/index.js
@@ -4,25 +4,62 @@ import classnames from 'classnames';
 
 import './styles.scss';
 
+const STYLES = {
+  box: {
+    width: 40,
+    height: 40
+  },
+  circle: {
+    fill: 'none',
+    stroke: '#000',
+    strokeWidth: 5,
+    strokeMiterlimit: 10
+  }
+};
+
 export class Spinner extends PureComponent {
   static propTypes = {
     className: PropTypes.string,
-    style: PropTypes.object
+    customClass: PropTypes.string,
+    style: PropTypes.shape({
+      box: PropTypes.object,
+      circle: PropTypes.object
+    })
   }
 
   static defaultProps = {
     className: null,
-    style: null
+    customClass: null,
+    style: {
+      box: {},
+      circle: {}
+    }
   }
 
   render() {
-    const { className, style } = this.props;
-    const customClass = classnames({ [className]: !!className });
+    const { className, customClass, style } = this.props;
+    const classNames = classnames({
+      'c-spinner': true,
+      [className]: !!className
+    });
+
+    const customClassNames = classnames({
+      'c-spinner': true,
+      [customClass]: !!customClass
+    });
 
     return (
-      <div styleName="c-spinner" style={style} className={customClass}>
-        <div styleName="spinner-box">
-          <div styleName="icon" />
+      <div styleName={classNames} className={customClassNames}>
+        <div styleName="spinner-box" style={{ ...STYLES.box, ...style.box }}>
+          <svg styleName="spinner-icon" viewBox="25 25 50 50">
+            <circle
+              {...{ ...STYLES.circle, ...style.circle}}
+              styleName="spinner-circle"
+              cx={50}
+              cy={50}
+              r={20}
+            />
+          </svg>
         </div>
       </div>
     );
---
+++ src/components/spinner/readme.md
@@ -0,0 +1,15 @@
+### Default
+```js
+<Spinner className="-relative" />
+```
+
+### Custom
+```js
+<Spinner
+  className="-relative"
+  style={{
+    box: { width: 20, height: 20 },
+    circle: { stroke: '#F00', strokeWidth: 8 }
+  }}
+/>
+```
