a40c413f97c2f99fe2b74c7c10d00b270e4576f4
Clément Prod'homme <contact@clementprodhomme.fr>
2021-01-26 14:28:19 +0100

    Update the webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const glob = require('glob');
 const webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
@@ -11,28 +11,19 @@ const env = process.env.NODE_ENV || 'development';
 const isDev = env === 'development';
 
 const config = {
-
   devtool: isDev ? 'cheap-eval-source-map' : false,
 
   mode: env,
 
   entry: {
-    icon: path.resolve(__dirname, 'src/components/icon/index.js'),
-    icons: path.resolve(__dirname, 'src/components/icons/index.js'),
-    legend: path.resolve(__dirname, 'src/components/legend/index.js'),
-    map: path.resolve(__dirname, 'src/components/map/index.js'),
-    tooltip: path.resolve(__dirname, 'src/components/tooltip/index.js'),
-    bundle: path.resolve(__dirname, 'src/components/index.js'),
-    spinner: path.resolve(__dirname, 'src/components/spinner/index.js'),
-    // datepicker: path.resolve(__dirname, 'src/components/datepicker/index.js'),
-    slider: path.resolve(__dirname, 'src/components/slider/index.js'),
-    timestep: path.resolve(__dirname, 'src/components/timestep/index.js')
+    bundle: path.resolve(__dirname, './src/components/index.js'),
   },
 
   output: {
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
     filename: '[name].js',
-    libraryTarget: 'commonjs2'
+    libraryTarget: 'commonjs2',
   },
 
   module: {
@@ -40,7 +31,7 @@ const config = {
       {
         test: /\.(jsx|js)?$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
@@ -51,13 +42,13 @@ const config = {
             options: {
               importLoaders: 1,
               includePaths: ['./node_modules']
-                .map(d => path.join(__dirname, d))
-                .map(g => glob.sync(g))
-                .reduce((a, c) => a.concat(c), [])
-            }
+                .map((d) => path.join(__dirname, d))
+                .map((g) => glob.sync(g))
+                .reduce((a, c) => a.concat(c), []),
+            },
           },
-          'postcss-loader'
-        ]
+          'postcss-loader',
+        ],
       },
       { test: /\.(jpg|jpeg|png|gif)$/, use: 'url-loader' },
       {
@@ -67,76 +58,55 @@ const config = {
             loader: MiniCssExtractPlugin.loader,
             options: {
               filename: isDev ? '[name].css' : '[name].[hash].css',
-              chunkFilename: isDev ? '[id].css' :  '[id].[hash].css'
-            }
+              chunkFilename: isDev ? '[id].css' : '[id].[hash].css',
+            },
           },
           {
             loader: 'css-loader',
             options: {
               modules: true,
               importLoaders: 1,
-              localIdentName: 'vizzuality__[local]'
-            }
+              localIdentName: 'vizzuality__[local]',
+            },
           },
           'postcss-loader',
           {
             loader: 'sass-loader',
             options: {
               includePaths: ['./node_modules', './src/css']
-                .map(d => path.join(__dirname, d))
-                .map(g => glob.sync(g))
-                .reduce((a, c) => a.concat(c), [])
-            }
-          }
-        ]
-      }
-    ]
+                .map((d) => path.join(__dirname, d))
+                .map((g) => glob.sync(g))
+                .reduce((a, c) => a.concat(c), []),
+            },
+          },
+        ],
+      },
+    ],
   },
 
-  externals: [
-    'react',
-    'react-dom',
-    'leaflet',
-    'moment'
-  ],
-
-  performance: {
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000,
-  },
+  externals: ['react', 'react-dom', 'leaflet', 'moment'],
 
   optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        uglifyOptions: {
-          ecma: 5,
-          output: { comments: false },
-          compress: { dead_code: true, drop_console: true }
-        },
-        sourceMap: true
-      }),
-      new OptimizeCSSAssetsPlugin({})
-    ]
+    usedExports: true,
+    minimize: true,
+    minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})],
   },
 
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(env)
+      'process.env.NODE_ENV': JSON.stringify(env),
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].css'
+      filename: '[name].css',
     }),
     new CompressionPlugin({
       asset: '[path].gz[query]',
       algorithm: 'gzip',
       test: /\.js$|\.css$|\.html$|\.eot?.+$|\.ttf?.+$|\.woff?.+$|\.svg?.+$/,
       threshold: 10240,
-      minRatio: 0.8
-    })
-  ]
-
+      minRatio: 0.8,
+    }),
+  ],
 };
 
 if (process.env.BUNDLE_ANALYZE) {
